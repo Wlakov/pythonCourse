1771e7c23df1dd0e3e640bf5c91c6aa8327bfe2b
Wlakov <koval0310@gmail.com>
2023-01-01 18:17:02 +0200

    Hometask(functions)

---
+++ lesson6/hometask1.py
@@ -0,0 +1,67 @@
+def display_menu():
+    print('Обрахунок комунальних послуг\n'
+          'Під час обрахунку використовуються перші 5 цифр на газовому лічильнику\n'
+          'Але БЕЗ нулів на початку')
+
+
+def calculate_pokazniki():
+    display_menu()
+    num1, num2 = get_pokazniki_from_user()
+    input_tariff = choose_tariff()
+    tariff(input_tariff, num1, num2)
+
+
+def choose_tariff(lower_bound: int = 1, upper_bound: int = 3) -> int:
+    while True:
+        try:
+            print("""Виберіть тариф(числом) 
+            1-Волинська область( 7.98 грн/м.куб) 
+            2-Дніпропетровська область( 8.46 грн/м.куб)
+            3-Київська область (9.95 грн/м.куб)""")
+            input_tariff = input('Виберіть : ')
+            input_tariff = int(input_tariff)
+            if input_tariff < lower_bound or input_tariff > upper_bound:
+                raise ValueError
+            return input_tariff
+        except Exception:
+            print('Error,try again')
+
+
+def tariff(input_tariff, num1, num2) -> int:
+    if input_tariff == 1:
+        x = (num2 - num1) * 7.98
+        print(f'Сума до сплати:{round(x, 2)} гривень')
+    elif input_tariff == 2:
+        x = (num2 - num1) * 8.46
+        print(f'Сума до сплати:{round(x, 2)} гривень')
+    elif input_tariff == 3:
+        x = (num2 - num1) * 9.95
+        print(f'Сума до сплати:{round(x, 2)} гривень')
+    else:
+        exit()
+
+
+# display_menu()
+# chosen = choose_tariff()
+# print(chosen)
+
+def get_pokazniki_from_user() -> int:
+    num1 = ''
+    num2 = ''
+    while True:
+        try:
+            num1 = input('Input past pokaznik: ')
+            num1 = int(num1)
+            num2 = input('Input present pokaznik: ')
+            num2 = int(num2)
+            if num1 > num2:
+                print('error')
+                continue
+            return num1, num2
+        except Exception:
+            print('Error,try again')
+
+
+if __name__ == '__main__':
+    while True:
+        calculate_pokazniki()
---
+++ lesson6/hometask2.py
@@ -0,0 +1,28 @@
+def get_sides_from_user() -> int:
+    while True:
+        try:
+            num1 = input('Input 1: ')
+            num1 = int(num1)
+            num2 = input('Input 2: ')
+            num2 = int(num2)
+            num3 = input('Input 3: ')
+            num3 = int(num3)
+            if (num1 + num2) < num3 or (num2 + num3) < num1 or (num1 + num3) < num2:
+                print('error')
+                continue
+            return num1, num2, num3
+        except Exception:
+            print('Error,try again')
+
+
+def calc_triangle() -> int:
+    side1, side2, side3 = get_sides_from_user()
+    p = (side1 + side2 + side3) / 2
+    s = pow(p * (p - side1) * (p - side2) * (p - side3), 1 / 2)
+    print(f'Периметр трикутника зі стронами {side1}, {side2}, {side3} дорівнює {p}')
+    print(f'Площа трикутника зі стронами {side1}, {side2}, {side3} дорівнює {s}')
+
+
+if __name__ == '__main__':
+    while True:
+        calc_triangle()
